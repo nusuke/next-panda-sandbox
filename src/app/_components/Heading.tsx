e59b8a459998054bc0d66295ae4edf7567f204e2
nusuke <nusukep@gmail.com>
2024-09-14 20:27:24 +0900

    fix comment

--- src/app/_components/Heading.tsx
+++ src/app/_components/Heading.tsx
@@ -1,21 +1,6 @@
 import { cva, RecipeVariantProps } from "../../../styled-system/css";
 import { RESULT } from "./const";
 
-/*
-If it's the same file, it works as expected with panda css, but,
-import from an external file will not be included in panda's AST.
-TODO computed value is played by no-dynamic-styling rule.
-
-pnpm panda debug
-open styled-system/debug/src__app___components__Heading.ast.json
-
-↓if same file panda ok, but I want to report lint because it is dynamic
-enum RESULT {
-  OK = "ok",
-  NG = "ng",
-}
-*/
-
 type P = {
   as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   text: string;
@@ -33,6 +18,19 @@ const heading = cva({
       secondary: { color: "white", backgroundColor: "orange" },
     },
     result: {
+      /*
+If it's the same file, it works as expected with panda css, but,
+import from an external file will not be included in panda's AST.
+TODO computed value is played by no-dynamic-styling rule.
+
+```sh
+pnpm panda debug
+open styled-system/debug/src__app___components__Heading.ast.json
+```
+
+computed Value"result" is empty.
+*/
+
       [RESULT.OK]: {
         fontSize: "2xl",
         border: "1px solid red",
